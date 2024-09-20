62957a9e9293c824c33a82147241669c9a59cb53
Emma <elune.lind@gmail.com>
2024-09-20 11:24:48 +0200

    Add project files.

---
+++ EgenDevopsTestAppBMIRäknare/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EgenDevopsTestAppBMIRäknare.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
